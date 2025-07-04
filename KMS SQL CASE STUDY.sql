e117f69c51474a6c156a3b549afc28dd0d30f59e
Brenda147 <onyeachorbrenda2018@gmail.com>
2025-07-04 22:53:41 +0100

    Update KMS SQL CASE STUDY.sql

--- KMS SQL CASE STUDY.sql
+++ KMS SQL CASE STUDY.sql
@@ -1,6 +1,6 @@
 ----- DSA Capstone Project 
 -----Kultra Mega Stores Inventory Case Study
------Candidate Name: Adeiye Akande 
+-----Candidate Name: Onyeachor Brenda 
 
 
 ----1. Product Category With The Highest Sale is Technology with Total sales of 5984248.18199998
